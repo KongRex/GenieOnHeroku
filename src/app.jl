cf9f10c00c0792041330e83899d8191b6f989779
KongRex <44677741+KongRex@users.noreply.github.com>
2021-08-12 07:32:16 +0200

    Update app.jl

--- src/app.jl
+++ src/app.jl
@@ -1,28 +1,19 @@
-using Genie, Genie.Router, Genie.Requests
-import Genie.Renderer.Json
-using JuMP #,uIpopt # Gurobi, CSV, DataFrames, 
 using GLPK
 using Gadfly
 
-
 function launchServer(port)
+
     Genie.config.run_as_server = true
     Genie.config.server_host = "0.0.0.0"
     Genie.config.server_port = port
-
-    println("port set to $(port)")
-
-    route("/") do
-        "Hi there!"
-    end
-
+	@@ -18,7 +119,9 @@ function launchServer(port)
     route("/echo", method = POST) do
-      message = jsonpayload()
-      (:echo => (message["message"] * " ") ^ message["repeat"]) |> json
+        message = jsonpayload()
+        #(:echo => (message["message"] * " ") ^ message["repeat"]) |> json
+        json(message)
     end
-    
+
     Genie.AppServer.startup()
+	
 end
-
 launchServer(parse(Int, ARGS[1]))
-
